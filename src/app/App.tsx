02cc14085bd5fae020522eb26e0e6676e0b4830b
Park Taehyeon <xogus28003@gmail.com>
2026-04-21 19:56:29 +0900

    1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { CampgroundManagement } from "./components/CampgroundManagement";
-import { CampgroundDetail } from "./components/CampgroundDetail";
-import { ForecastAnalysis } from "./components/ForecastAnalysis";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Layout } from "./components/Layout";
 import { MainHome } from "./components/MainHome";
+import { CampgroundManagement } from "./components/CampgroundManagement";
+import { StatisticsAnalysis } from "./components/StatisticsAnalysis";
+import { ForecastAnalysis } from "./components/ForecastAnalysis";
+import { ReviewManagement } from "./components/ReviewManagement";
+import { CampgroundDetail } from "./components/CampgroundDetail";
+import { Dashboard } from "./components/Dashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainHome />} />
-        <Route path="/forecast" element={<ForecastAnalysis />} />
-        <Route path="/campground/:id" element={<CampgroundDetail />} />
+        <Route element={<Layout />}>
+          <Route index element={<Navigate to="mainhome" replace />} />
+          <Route path="mainhome" element={<MainHome />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="campground" element={<CampgroundManagement />} />
+          <Route path="campground/:id" element={<CampgroundDetail />} />
+          <Route path="statistics" element={<StatisticsAnalysis />} />
+          <Route path="forecast" element={<ForecastAnalysis />} />
+          <Route path="review" element={<ReviewManagement />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
